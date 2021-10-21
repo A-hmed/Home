e8b21b8e6fb30d3d5199a4aec7b83352675609a9
Ahmed Nabil <an0088039@gmail.com>
2021-10-21 19:12:11 +0200

    Finished LoginScreen Ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:git_testing/LoginScreen.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      home: LoginScreen(),
     );
   }
 }
